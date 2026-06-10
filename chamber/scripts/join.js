b635ed539883588ca3589908917a2a1fee479d04
LWAZILUBISI <lwaziluigi@gmail.com>
2026-06-10 22:06:09 +0200

    join page fix

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -56,3 +56,10 @@ dialogs.forEach(dialog => {
         }
     });
 });
+
+const menuBtn = document.querySelector("#menuBtn");
+const navMenu = document.querySelector("#navMenu");
+
+menuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+});
